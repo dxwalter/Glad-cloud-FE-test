fb50e0ad4c43b827a27408884c50d0e44c83a746
Daniel Walter <realdanielwalter@gmail.com>
2022-08-01 09:17:31 +0100

    headless select form

--- src/components/country-list.vue
+++ src/components/country-list.vue
@@ -1,49 +1,116 @@
 <template>
-  <div>
-    <select
-      class="login-form-input mb-2"
-      @change="emitValue"
-      v-model="selectedCountry"
-    >
-      <option selected value="">Select country</option>
-      <option
-        v-for="data in listOfCountries.value"
-        :key="data.id"
-        :value="data.id"
+  <Combobox v-model="selected" class="mb-2">
+    <div class="">
+      <div class="">
+        <ComboboxInput
+          class="w-100 login-form-input"
+          :displayValue="(country) => country.name"
+          @change="query = $event.target.value"
+          v-model="selectedCountry"
+          placeholder="Select country"
+        />
+        <ComboboxButton
+          class="absolute inset-y-0 right-0 flex items-center pr-2"
+        >
+        </ComboboxButton>
+      </div>
+      <TransitionRoot
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        @after-leave="query = ''"
       >
-        {{ data.name }}
-      </option>
-    </select>
-  </div>
+        <ComboboxOptions
+          class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-index-1"
+        >
+          <div
+            v-if="filteredCountry.length === 0 && query !== ''"
+            class="relative cursor-default select-none py-2 px-4 text-gray-700"
+          >
+            Nothing found.
+          </div>
+
+          <ComboboxOption
+            v-for="country in filteredCountry"
+            as="template"
+            :key="country.id"
+            :value="country"
+            v-slot="{ selected, active }"
+          >
+            <li
+              class="relative cursor-default select-none py-2 pl-10 pr-4"
+              :class="{
+                'bg-teal-600 text-white': active,
+                'text-gray-900': !active,
+              }"
+            >
+              <span
+                class="block truncate"
+                :class="{ 'font-medium': selected, 'font-normal': !selected }"
+              >
+                {{ country.name }}
+              </span>
+              <span
+                v-if="selected"
+                class="absolute inset-y-0 left-0 flex items-center pl-3"
+                :class="{ 'text-white': active, 'text-teal-600': !active }"
+              >
+              </span>
+            </li>
+          </ComboboxOption>
+        </ComboboxOptions>
+      </TransitionRoot>
+    </div>
+  </Combobox>
 </template>
-<script lang="ts">
-import { defineComponent, ref, inject, computed, ComputedRef } from "vue";
+
+<script setup>
+import { ref, computed, watch } from "vue";
 import { countryStore } from "@/stores/countries-store";
 import { storeToRefs } from "pinia";
 
-export default defineComponent({
-  name: "CountryDropdownComponent",
-  props: {},
-  setup(_, { emit }) {
-    const countryStoreData = countryStore();
-    const { countryData } = storeToRefs(countryStoreData);
-    const listOfCountries = computed(() => countryData);
-    const selectedCountry = ref<string | number>("");
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxButton,
+  ComboboxOptions,
+  ComboboxOption,
+  TransitionRoot,
+} from "@headlessui/vue";
 
-    const emitValue = () => {
-      if (selectedCountry.value.toString().length) {
-        // emit data
-        emit("selected-country", {
-          countryId: selectedCountry.value,
-        });
-      }
-    };
+const emit = defineEmits(["selectedCountry"]);
 
-    return {
-      listOfCountries,
-      selectedCountry,
-      emitValue,
-    };
+const countryStoreData = countryStore();
+const { countryData } = storeToRefs(countryStoreData);
+const listOfCountries = computed(() => [
+  {
+    name: "Select country",
+    id: 0,
   },
+  ...countryData.value,
+]);
+const selectedCountry = ref("");
+
+const selected = ref(listOfCountries.value[0]);
+const query = ref("");
+
+const filteredCountry = computed(() => {
+  return query.value === ""
+    ? listOfCountries.value
+    : listOfCountries.value.filter((country) =>
+        country.name
+          .toLowerCase()
+          .replace(/\s+/g, "")
+          .includes(query.value.toLowerCase().replace(/\s+/g, ""))
+      );
+});
+
+// watch works directly on a ref
+watch(selected, async (newValue, oldValue) => {
+  if (newValue.id > 0) {
+    emit("selectedCountry", {
+      countryId: newValue.id,
+    });
+  }
 });
 </script>
